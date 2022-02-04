a861c5a64d5f691a2335b24757240b1f529be0d4
pl0kis <93941496+pl0kis@users.noreply.github.com>
2022-02-04 11:35:22 +0300

    Add files via upload

---
+++ pract1.js
@@ -0,0 +1,50 @@
+    // // Задание 1 
+    // let a = 1
+
+    // if (a=a){
+    //     console.log('Да')
+    // } else if (a!=a) {
+    //     console.log('Нет')
+    // } else {
+    //     console.log('Логично')
+    // }
+
+    // a==1?console.log('а = 1'):console.log('a != 1')
+
+    // Задание 2
+
+    // let array = [5,-3,'Apple',10]
+    // let i = 0
+    // for(arr of array){
+    //     i+=1
+    //     console.log('number', arr)
+    //     if (i==3){
+    //         break
+    //     }
+    // }
+
+    // Задание 3 
+
+    // let login = 'Сотрудник'
+
+    // login=='Сотрудник'?console.log('Привет'):login=='Директор'?console.log('Здравствуйте'):login ==''?console.log('Нет логина'):console.log('')
+
+    // Задание 4
+
+
+
+    // Задание 5
+
+    let day = 'Январь'
+
+    switch (day){
+        case 'Январь':
+            console.log('1 месяц дней 31')
+            break
+        case 'Февраль':
+            console.log('2')
+            break
+        case 'Март':
+            console.log('3')
+            break
+    }
