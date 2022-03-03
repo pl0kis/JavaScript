295caf86ac2fd1430ad4b0b8ef3000e6ae9ab577
pl0kis <93941496+pl0kis@users.noreply.github.com>
2022-03-03 14:53:43 +0300

    Add files via upload

---
+++ kuznetsov/js.js
@@ -0,0 +1,13 @@
+// let but = document.querySelector('.square')
+
+// but.addEventListener("mouseover",() => {
+//     let square = document.querySelector(".square")
+//     square.style = "background-color:green"
+// })
+
+// but.addEventListener('mouseout',() => {
+//     square = document.querySelector('.square')
+//     square.style = "background-color:black"
+// })
+
+let but = document.querySelector('.square')
